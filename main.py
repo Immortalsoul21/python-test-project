9399ceda67d101ae060af2013dcb609ffd832a65
Tushar Mondal <129579596+Immortalsoul21@users.noreply.github.com>
2023-04-13 09:12:58 +0000

    written program for greeting

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("hello world")
+
+name=input("enter your name:")
+print("hello" , name)
+time=float(input("enter the time:"))
+if time<12.00:
+  print("good morning ",name)
+elif time > 12.00 and time<16.00:
+  print("good afternoon" , name)
+else: 
+  print("good night" ,name)
